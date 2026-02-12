34aa32de63a92f36b715ab807103dda736707ad3
Mythri panugatla <mp9816@srmist.edu.in>
2026-02-12 18:35:05 +0530

    Add tuple access, addition, and iteration examples
    Demonstrate tuple element access, addition, and iteration.

---
+++ 00-python-basics/0.10 tuple_problems1.py
@@ -0,0 +1,18 @@
+# access elements of tuple
+my_tuple = (10, 20, 30, 40, 50)
+print("First element:", my_tuple[0])
+print("Last element:", my_tuple[-1])
+
+# add elements to tuple
+n = 4
+main_tuple = (10, 40)
+for i in range(0,n):
+    new_value = input("Enter value: ")
+    main_tuple = main_tuple + (new_value,)
+    print(main_tuple)
+# iterate through tuple
+    for i in range(0,len(main_tuple)):  
+        print(f"Element at index {i}: {main_tuple[i]}")
+    
+
+    
