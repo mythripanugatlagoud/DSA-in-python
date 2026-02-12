5e2b75092cc2410e418e2b1a640e6b5b43cd4c67
Mythri panugatla <mp9816@srmist.edu.in>
2026-02-12 18:36:01 +0530

    Implement tuple index and count functionality
    Added a script to find the index and count occurrences of an item in a tuple.

---
+++ 00-python-basics/0.11 tuple_problem2.py
@@ -0,0 +1,18 @@
+#find index of an item in a tuple
+tuple = (1, 2, 3, 4, 5)
+find= 3
+for i in range(len(tuple)):
+    if (find == tuple[i]):
+        print("element index is", i)
+        break
+
+# Output: element index is 2
+# count occurrences of an item in a tuple
+tuple = (1, 2, 3, 4, 5, 3, 3)
+find= 3
+count = 0
+for i in range(len(tuple)):
+    if (find == tuple[i]):
+        count += 1
+print("element count is", count)
+# Output: element count is 3
